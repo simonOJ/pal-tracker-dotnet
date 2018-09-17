nwade/pal-tracker-dotnet-v2

Simple .NET Core Web API app

src/PalTracker/WelcomeController.cs
@@ -1,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : Controller
+    {
+        [HttpGet]
+        public string SayHello() => "hello";
+    }
+}

simonOJ/pal-tracker-dotnet

src/PalTracker/WelcomeController.cs
@@ -1,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello() => "hello";
+    }
+}